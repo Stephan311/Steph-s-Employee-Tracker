330bede8a5a379a3dd43c6e1384a79c3b0262200
stephan du Plooy <sdpphotography1@gmail.com>
2021-05-18 08:45:19 -0400

    Conected Database to Node

--- employeecode.js
+++ employeecode.js
@@ -1,2 +1,29 @@
-const inquirer = require('inquirer');
+// const inquirer = require('inquirer');
 const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    
+    port: 3306,
+
+    user: 'root',
+
+
+    password: 'Seahawks2014',
+    database: 'employees_db',
+
+});
+
+const afterConnection = () => {
+    connection.query('SELECT * FROM department', (err, res) => {
+        if (err) throw (err);
+        console.log(res);
+        connection.end();
+    });
+};
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+    afterConnection();
+})
--- seed.sql
+++ seed.sql
@@ -4,21 +4,47 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
+id INTEGER(20) AUTO_INCREMENT NOT NULL,
 name VARCHAR(30) NOT NULL,
-id INT PRIMARY KEY
+PRIMARY KEY (id)
 );
+
+-- upate data values inside table column
+-- UPDATE department SET name = "Andy" WHERE id = 2;
+-- DELETE FROM department WHERE id = 2;
+
+-- insert data into table columns
+INSERT INTO department (name) VALUE ("SLT");
+SELECT * FROM department;
 
 CREATE TABLE role (
-id INT PRIMARY KEY,
+id INTEGER(20) AUTO_INCREMENT NOT NULL,
 title VARCHAR(30),
 salary DECIMAL,
-department_id INT
+department_id INT,
+PRIMARY KEY (id)
 );
 
+INSERT INTO role (title, salary, department_id) VALUE ("CEO", 20, 1);
+SELECT * FROM role;
+
 CREATE TABLE employee (
-id INT PRIMARY KEY,
+id INTEGER(20) AUTO_INCREMENT NOT NULL,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
-manager_id INT
+manager_id INT,
+PRIMARY KEY (id)
 );
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+ VALUE ("Stephan", "du Plooy", "1", "1");
+SELECT * FROM employee;
+
+SELECT name 
+FROM department
+INNER JOIN employee ON department.id = employee.id;
+
+SELECT department_id
+FROM role
+INNER JOIN employee ON role.id = employee.id
